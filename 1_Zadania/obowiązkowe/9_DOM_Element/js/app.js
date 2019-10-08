6019dc027b8e3e9498bf27b7216f9a773685f012
zer0xday <majcher10@gmail.com>
2019-10-08 20:40:16 +0200

    9th solved

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -9,4 +9,103 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    // zad 0
+    function getDatasInfo(elements) {
+        const newArray = [];
+
+        for (let link of elements) {
+            newArray.push(link.getAttribute('data-color'));
+        }
+
+        return newArray;
+    };
+
+    const linksAttributes = getDatasInfo(links);
+    // console.log(linksAttributes);
+
+    // zad 1
+    // console.log(homeElement, childElements, banner, blocks, links);
+
+    function outputCollections(collection) {
+        for (let item of collection) {
+            // console.log(
+            //     `tag: ${item.tagName}`, 
+            //     `class: ${item.className ? item.className : 'none'}`
+            // );
+        }
+    };
+
+    // outputCollections(childElements);
+    // outputCollections(blocks);
+    // outputCollections(links);
+
+    // zad 2
+    // innerHTML bierze pod uwagę tylko dzieci diva o klasie 'block'
+    // natomiast outerHTML zwraca dzieci wraz z elementem parent o klasie 'block'
+    for (let item of blocks) {
+        // console.log(
+        //     `innerHTML: ${item.innerHTML}`, 
+        //     `outerHTML: ${item.outerHTML}`
+        // );
+
+        item.innerHTML = "Nowa wartość diva o klasie blocks";
+        // console.log(item.innerHTML);
+        // elementy wewnatrz diva .block zostały zamienione na ustawioną przez nas powyżej wartość
+        // zamiast podwójnych znaczników <a> zadeklarowanych uprzednio w HTML
+        // uzyskamy w divie napis zadeklarowany w item.innerHTML
+    };
+
+    // zad 3
+    const mainFooter = document.querySelector('#mainFooter');
+    // console.log(mainFooter);
+    function getId(element) {
+        return element.id;
+    };
+
+    const mainFooterId = getId(mainFooter);
+    // console.log(mainFooterId);
+
+    // zad 4
+    function getTags(elements) {
+        const newArray = [];
+        
+        for (let element of elements) {
+            newArray.push(element.tagName);
+        }
+
+        return newArray;
+    };
+
+    const elementsTags = getTags(childElements);
+    // console.log(elementsTags);
+
+    // zad 5
+    function getClassInfo(element) {
+        const newArray = [];
+
+        for (els of element.childNodes) {
+            if (els.className !== undefined && els.className !== '') {
+                newArray.push(els.className);
+            }
+        };
+
+        return newArray;
+    };
+
+    const elementsClassNames = getClassInfo(banner);
+    console.log(elementsClassNames);
+
+    // zad 6
+    const listElements = document.querySelectorAll('nav > ul > li');
+    console.log(listElements);
+
+    function setDataDirection(elements) {
+        for (let element of elements) {
+            if (!element.hasAttribute('data-direction')) {
+                element.setAttribute('data-direction', 'top');
+            }
+        }
+    };
+
+    setDataDirection(listElements);
 });
