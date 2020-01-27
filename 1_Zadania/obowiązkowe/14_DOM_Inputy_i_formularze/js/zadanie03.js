eb963eec1705c75615d668e70b4079880013b627
zer0xday <majcher10@gmail.com>
2020-01-27 19:23:37 +0100

    14.03 solved

--- 1_Zadania/obowiązkowe/14_DOM_Inputy_i_formularze/js/zadanie03.js
+++ 1_Zadania/obowiązkowe/14_DOM_Inputy_i_formularze/js/zadanie03.js
@@ -1,16 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     class BoardManager {
         constructor() {
-            this.board = document.querySelector("table.table");
+            this.board = document.querySelector("table.table > tbody");
             this.form = document.querySelector('form');
         }
 
         validateTeamNames(name1, name2) {
-            if (name1 === name2) {
-                return false;
-            }
-
-            return true;
+            return name1 !== name2;
         }
 
         validateScore(_score) {
@@ -20,15 +16,40 @@ document.addEventListener('DOMContentLoaded', () => {
                 return false;
             }
 
-            if (score < 0) {
-                return false;
-            }
+            return score >= 0;
+        }
 
-            return true;
+        _createColumn(text) {
+            let td = document.createElement('td');
+            td.innerHTML = text;
+            return td;
+        }
+
+        _createRow(data) {
+            let row = document.createElement("tr");
+            let team1 = this._createColumn(data.firstTeam.value);
+            let team2 = this._createColumn(data.secondTeam.value);
+            let score = this._createColumn(
+                `${data.firstTeamPoints.value} - ${data.secondTeamPoints.value}`
+            );
+
+            row.appendChild(team1);
+            row.appendChild(team2);
+            row.appendChild(score);
+            return row;
         }
 
         addScoreToBoard(data) {
+            const { board } = this;
+            let row = this._createRow(data);
 
+            board.appendChild(row);
+        }
+
+        clearForm(data) {
+            for (let input in data) {
+                data[input].value = "";
+            }
         }
 
         handleForm() {
@@ -40,15 +61,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 const validators = [];
                 let passed = true;
                 const data = {
-                    firstTeam: document.querySelector('input#team1').value,
-                    secondTeam: document.querySelector('input#team2').value,
-                    firstTeamPoints: document.querySelector('input#points1').value,
-                    secondTeamPoints: document.querySelector('input#points2').value,
-                }
+                    firstTeam: document.querySelector('input#team1'),
+                    secondTeam: document.querySelector('input#team2'),
+                    firstTeamPoints: document.querySelector('input#points1'),
+                    secondTeamPoints: document.querySelector('input#points2'),
+                };
                 
-                validators.push(this.validateTeamNames(data.firstTeam, data.secondTeam));
-                validators.push(this.validateScore(data.firstTeamPoints));
-                validators.push(this.validateScore(data.secondTeamPoints));
+                validators.push(this.validateTeamNames(data.firstTeam.value, data.secondTeam.value));
+                validators.push(this.validateScore(data.firstTeamPoints.value));
+                validators.push(this.validateScore(data.secondTeamPoints.value));
 
                 validators.forEach((validator) => {
                     if (!validator) {
@@ -56,14 +77,17 @@ document.addEventListener('DOMContentLoaded', () => {
                     }
                 });
 
-                console.log(passed);
+                if (passed) {
+                    this.addScoreToBoard(data);
+                    this.clearForm(data);
+                }
             });
         }
 
         init() {
             this.handleForm();
         }
-    };
+    }
 
     const boardManager = new BoardManager;
     boardManager.init();
